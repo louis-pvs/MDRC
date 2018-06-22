53091d2d7ac8593433e325c410a9255988f5adad
@louis-pvs <design.vincent.louis@gmail.com>
2018-06-22 15:37:54 +0800

    deploying demo file for docz

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'lib/button/Button.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "lib-button-button" */ 'lib/button/Button.mdx'),
-  'lib/icon/Icon.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "lib-icon-icon" */ 'lib/icon/Icon.mdx'),
-}
